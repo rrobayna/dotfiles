b32a97d6ed99768c4b4aa1f8ad528660a87c07c5
Rafael Robayna <rrobayna@gmail.com>
2014-07-18 21:12:19 -0400

    Removed unused files Cleaned up the install script Improved the README file

--- install.sh
+++ install.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 
-# Create the vim home folder
-if [ ! -d $HOME/.vim/bundle ]; then
-    mkdir -p $HOME/.vim/bundle
-    echo "dosen't exist"
-else
-    echo "exists"
-fi
-
-# @todo check if I need this?
-git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-
 # Install configs
-# @todo replace this with a for read file that moves each file out
-cp $PWD/configs/bundles.vim $HOME/.vim/bundles.vim
+echo "Installing configs to $HOME"
 cp $PWD/configs/vimrc $HOME/.vimrc
 cp $PWD/configs/tmux.conf $HOME/.tmux.conf
 cp $PWD/configs/gitconfig $HOME/.gitconfig
 
-# Install vim plugins
-# check if this install can be performed with the vimrc?
-vim -u $HOME/.vim/bundles.vim +PluginInstall +qall
+# Install Vundle Plugin Manager for Vim 
+echo "Installing Vundle Plugin Manager for Vim..."
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+echo "Installing Vim plugins..."
+vim +PluginInstall +qall
+
+echo "Config installation complete!"
