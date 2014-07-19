c5f0a537ece19b3e030034e498e22abf64ef65cd
Rafael Robayna <rrobayna@gmail.com>
2014-07-18 21:15:03 -0400

    Renamed save to backup Fixed status message

--- install.sh
+++ install.sh
@@ -13,4 +13,4 @@ git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 echo "Installing Vim plugins..."
 vim +PluginInstall +qall
 
-echo "Config installation complete!"
+echo "Installation complete!"
