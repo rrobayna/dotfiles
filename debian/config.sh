163e6cdd777218874b3db1a4f7ceb4f796a76ffa
Rafael Robayna <rrobayna@gmail.com>
2014-10-02 05:53:34 -0400

    updated debian install scripts

--- debian/config.sh
+++ debian/config.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+backports="deb http://ftp.debian.org/debian wheezy-backports main contrib non-free"
+bpexists=$(grep "$backports" /etc/apt/sources.list | wc -l)
+if [ $bpexists -eq 0 ]; then 
+	echo "Adding backports repository"
+	echo "$backports" >> /etc/apt/sources.list
+fi
 
 echo "Setting clock"
-sudo cp /usr/share/zoneinfo/America/New_York /etc/localtime
+cp /usr/share/zoneinfo/America/New_York /etc/localtime
+
 echo "Setting bash as default shell"
 chsh -s /bin/bash
--- debian/devtools.sh
+++ debian/devtools.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+
 apt-get update
-apt-get install -y vim-nox tmux git wget exuberant-ctags make
+apt-get install -y vim-nox tmux git wget exuberant-ctags make jq curl
